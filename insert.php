d0dea25b6d6d646bcaaa83d71efc7fe3e166029a
Guiiihenriq <gyuhpb@gmail.com>
2021-11-02 19:18:51 +0000

    adicionando o bootstrap

--- insert.php
+++ insert.php
@@ -19,14 +19,21 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     <title>Novo Gênero</title>
 </head>
     <body>
+        <main class="container">
         <h1>Novo Gênero</h1>
         <form action="insert.php" method="post">
+            <div class="from-group">
             <label for="nome">Nome do Gênero</label>
-            <input type="text" required name="nome" />
-            <button type="submit">Salvar</button>
+            <input class="form-control" type="text" required name="nome" />
+            </div>
+            <br/>
+            <a class="btn btn-secondary" href="index.php" >Voltar</a>
+            <button class="btn btn-success" type="submit">Salvar</button>
         </form>
+        </main>
     </body>
 </html>
